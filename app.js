4317f2c38a1909ab7eb09777e4bde7ec0bfe35d7
Jaewon <choijawon99@gmail.com>
2022-05-20 15:16:05 +0900

    express JADE 연습

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
 var express = require('express');
 var app = express();
-app.use(express.static('public')); //정적인 파일 사용 가능
+
+app.locals.pretty = true;
+app.set('view engine', 'jade'); //jade 템플릿 엔진과 express를 연결
+app.set('views', './views')
+app.use(express.static('public')); 
+/*
+    정적인 파일 사용 가능
+    node 서버 실행중에 정적인 파일 수정 가능
+*/
+app.get('/template', function(req, res){
+    res.render('temp', {time: Date(), _title:'Jade'}); //jade 파일에 변수 전달
+})
 
 app.get('/',function(req, res){ // 홈 / 으로 접속했을때 callback 함수 실행 
     res.send('Hello home page');
 });
+
+app.get('/dynamic',function(req, res){
+    var lis = '';
+    for(var i=0;i<5;i++){
+        lis = lis + '<li>coding</li>';
+    }
+    var time = Date();
+    var output = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title></title>
+        </head>
+        <body>
+        <ul>
+            Hello Dynamic!
+            ${lis}
+        </ul>
+        ${time}
+        </body>
+    </html>
+    `;
+    res.send(output)
+})
 
 app.get('/route',function(req,res){
     res.send('Hello Router, <img src="/route.png">');
